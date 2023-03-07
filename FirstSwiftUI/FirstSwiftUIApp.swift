f980215a7aa88704140d55fa2c87ea32c7b04b1e
filosuf <filosuf@mail.ru>
2023-03-07 16:13:36 +0300

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by Filosuf on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
